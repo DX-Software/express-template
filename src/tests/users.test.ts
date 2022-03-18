f89a15d695456259bf6b80527c43f3a25ea025c0
lukesamkharadze <lukesamkharadze@gmail.com>
2022-03-18 22:11:05 +0400

    style: lint

--- src/tests/users.test.ts
+++ src/tests/users.test.ts
@@ -1,41 +1,43 @@
-import request from 'supertest';
-import App from '@/app';
-import { CreateUserDto } from '@dtos/users.dto';
-import { User } from '@interfaces/users.interface';
-import userModel from '@models/users.model';
-import UserRoute from '@routes/users.route';
+import request from "supertest";
+import App from "@/app";
+import { CreateUserDto } from "@dtos/users.dto";
+import { User } from "@interfaces/users.interface";
+import userModel from "@models/users.model";
+import UserRoute from "@routes/users.route";
 
 afterAll(async () => {
-  await new Promise<void>(resolve => setTimeout(() => resolve(), 500));
+  await new Promise<void>((resolve) => setTimeout(() => resolve(), 500));
 });
 
-describe('Testing Users', () => {
-  describe('[GET] /users', () => {
-    it('response statusCode 200 / findAll', () => {
+describe("Testing Users", () => {
+  describe("[GET] /users", () => {
+    it("response statusCode 200 / findAll", () => {
       const findUser: User[] = userModel;
       const usersRoute = new UserRoute();
       const app = new App([usersRoute]);
 
-      return request(app.getServer()).get(`${usersRoute.path}`).expect(200, { data: findUser, message: 'findAll' });
+      return request(app.getServer()).get(`${usersRoute.path}`).expect(200, { data: findUser, message: "findAll" });
     });
   });
 
-  describe('[GET] /users/:id', () => {
-    it('response statusCode 200 / findOne', () => {
+  describe("[GET] /users/:id", () => {
+    it("response statusCode 200 / findOne", () => {
       const userId = 1;
-      const findUser: User = userModel.find(user => user.id === userId);
+      const findUser: User = userModel.find((user) => user.id === userId);
       const usersRoute = new UserRoute();
       const app = new App([usersRoute]);
 
-      return request(app.getServer()).get(`${usersRoute.path}/${userId}`).expect(200, { data: findUser, message: 'findOne' });
+      return request(app.getServer())
+        .get(`${usersRoute.path}/${userId}`)
+        .expect(200, { data: findUser, message: "findOne" });
     });
   });
 
-  describe('[POST] /users', () => {
-    it('response statusCode 201 / created', async () => {
+  describe("[POST] /users", () => {
+    it("response statusCode 201 / created", async () => {
       const userData: CreateUserDto = {
-        email: 'example@email.com',
-        password: 'password',
+        email: "example@email.com",
+        password: "password",
       };
       const usersRoute = new UserRoute();
       const app = new App([usersRoute]);
@@ -44,12 +46,12 @@ describe('Testing Users', () => {
     });
   });
 
-  describe('[PUT] /users/:id', () => {
-    it('response statusCode 200 / updated', async () => {
+  describe("[PUT] /users/:id", () => {
+    it("response statusCode 200 / updated", async () => {
       const userId = 1;
       const userData: CreateUserDto = {
-        email: 'example@email.com',
-        password: 'password',
+        email: "example@email.com",
+        password: "password",
       };
       const usersRoute = new UserRoute();
       const app = new App([usersRoute]);
@@ -58,14 +60,16 @@ describe('Testing Users', () => {
     });
   });
 
-  describe('[DELETE] /users/:id', () => {
-    it('response statusCode 200 / deleted', () => {
+  describe("[DELETE] /users/:id", () => {
+    it("response statusCode 200 / deleted", () => {
       const userId = 1;
-      const deleteUser: User[] = userModel.filter(user => user.id !== userId);
+      const deleteUser: User[] = userModel.filter((user) => user.id !== userId);
       const usersRoute = new UserRoute();
       const app = new App([usersRoute]);
 
-      return request(app.getServer()).delete(`${usersRoute.path}/${userId}`).expect(200, { data: deleteUser, message: 'deleted' });
+      return request(app.getServer())
+        .delete(`${usersRoute.path}/${userId}`)
+        .expect(200, { data: deleteUser, message: "deleted" });
     });
   });
 });
